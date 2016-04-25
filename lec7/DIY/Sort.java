0091b81d0a53a618a166037b50be6097a47a1cfb
kremenovic96 <ranko.kremenovic@gmail.com>
2016-04-25 23:01:05 +0200

    cant load main cass

--- lec7/DIY/Sort.java
+++ lec7/DIY/Sort.java
@@ -2,7 +2,23 @@ public class Sort {
 
 	/** Returns input strings in sorted order. 
 	  * Non-destructive. */
-	public static String[] Sort(String[] inputs) {
+ public static int indexOfSmallest(String[] inputs)
+  {
+
+
+
+  }
+ private public String[] sort(String[] inputs, int k)
+ {
+
+
+
+ }
+	public static String[] sort(String[] inputs) {
+         sort(inputs,0); 
+
+
+
 
 	}
 
--- lec7/DIY/TestSort.java
+++ lec7/DIY/TestSort.java
@@ -1,6 +1,14 @@
+/*import org.junit.Test;
+import static org.junit.Assert.*;*/
 /** Tests the methods of the Sort class */
 public class TestSort {
-
+ public static void testSort(){
+String[] inputs = {"milvia","zero","josh","trump"};
+String[] expected={"josh","milvia","trump","zero"};
+String[] actual=Sort.sort(inputs);
+ org.junit.Assert.assertArrayEquals(expected,actual);
+ }
 	public static void main(String[] args) {
+ testSort();
 	}
 } 
